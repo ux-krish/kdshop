7e64012ca54b5feaa212bcc6510aea9d0d0e2d56
ux-krish <krishnendu.rantu@gmail.com>
2024-03-27 02:01:12 +0530

    new changes v16

--- src/components/weather/WeatherCard.js
+++ src/components/weather/WeatherCard.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import { FaWind, FaTint, FaThermometerHalf, FaLocationArrow ,FaMapPin   } from 'react-icons/fa';
 import { GiPositionMarker } from 'react-icons/gi'
+import LoadingUi from '../common/LoadingUi';
 
 
 const WeatherCard = () => {
@@ -21,6 +22,8 @@ const WeatherCard = () => {
 
   const [backgroundImage, setBackgroundImage] = useState('');
   const [selectedDayIndex, setSelectedDayIndex] = useState(null);
+
+  const [currentHour, setCurrentHour] = useState(new Date().getHours());
 
 
 
@@ -66,7 +69,7 @@ const WeatherCard = () => {
       const response = await fetch(apiUrl);
       const data = await response.json();
       setWeather(data);
-      console.log(data);
+      //console.log(data);
       setCurrentTemp(data.currentConditions.feelslike);
       setHumidity(data.currentConditions.humidity);
       setWindSpeed(data.currentConditions.windspeed);
@@ -168,6 +171,10 @@ const WeatherCard = () => {
     }
   };
 
+
+
+  
+
   return (
     <div className="rounded-md shadow-md">
       <h1 className="text-2xl mb-4 text-slate-200 font-semibold">Weather Updates</h1>
@@ -194,213 +201,103 @@ const WeatherCard = () => {
        
       </div>
       {loading ? (
-      <div className="border border-slate-800 rounded-md p-4 w-full mx-auto">
-        <div className="animate-pulse flex space-x-4 ">
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-          </div>
-          <div className="animate-pulse flex space-x-4 mt-4">
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-          </div>
-          <div className="animate-pulse flex space-x-4 mt-4">
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-          </div>
-          <div className="animate-pulse flex space-x-4 mt-4">
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-          </div>
-          <div className="animate-pulse flex space-x-4 mt-4">
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-          </div>
-          <div className="animate-pulse flex space-x-4 mt-4">
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-            <div className="flex-1 space-y-6 py-1">
-              <div className="space-y-3">
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                  <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                </div>
-                <div className="h-2 bg-slate-700 rounded"></div>
-              </div>
-            </div>
-          </div>
-       </div>
+        <LoadingUi />
       ) : (
         <>
-        
-        <p className="mb-4 text-slate-400 font-light flex items-start"><span className='font-bold mr-2 ms-2 relative top-[3px]'><FaMapPin  /></span> {location}</p>
-        <div
-          style={{
-            width: '100%',
-            minHeight: '300px',
-            background: `url(${process.env.PUBLIC_URL}/${backgroundImage}) no-repeat center center fixed`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center'
-          }} 
-          className="flex w-full overflow-hidden rounded-md shadow-md">
+          <p className="mb-4 text-slate-400 font-light flex items-start"><span className='font-bold mr-2 ms-2 relative top-[3px]'><FaMapPin  /></span> {location}</p>
+          <div
+            style={{
+              width: '100%',
+              minHeight: '300px',
+              background: `url(${process.env.PUBLIC_URL}/${backgroundImage}) no-repeat center center fixed`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center'
+            }} 
+            className="flex w-full overflow-hidden rounded-md shadow-md">
 
-          <div className='flex flex-wrap items-center justify-between p-4  min-h-full bg-gray-950/70 w-full '>
-            <div className="flex gap-3 flex-wrap grow flex-col mb-auto">
-                <p className="text-slate-200 flex flex-wrap items-start text-6xl font-extrabold mt-2 w-full grow justify-between">
-                  <span className='block w-full font-light text-sm px-4'>Feels Like</span>
-                  <span className='flex items-center'>
-                    <FaThermometerHalf className="mr-1 text-rose-600 text-5xl relative top-[2px]" /> 
-                    {Math.round(currentTemp)} 
-                    <sup className='text-[20px] relative -top-[12px] left-0'>°C</sup>
-                    <span className='flex items-center text-[18px] relative -top-[12px] ms-4'>
-                      <FaTint className="mr-[4px] text-sky-300 text-[14px]" />
-                      {currentDew}<sup className='text-[8px] relative -top-[3px]'>°C</sup>
+              <div className='flex flex-wrap items-center justify-between p-4  min-h-full bg-gray-950/70 w-full '>
+                <div className="flex gap-3 flex-wrap grow flex-col mb-auto">
+                  <p className="text-slate-200 flex flex-wrap items-start text-6xl font-extrabold mt-2 w-full grow justify-between">
+                    <span className='block w-full font-light text-sm px-4'>Feels Like</span>
+                    <span className='flex items-center'>
+                      <FaThermometerHalf className="mr-1 text-rose-600 text-5xl relative top-[2px]" /> 
+                      {Math.round(currentTemp)} 
+                      <sup className='text-[20px] relative -top-[12px] left-0'>°C</sup>
+                      <span className='flex items-center text-[18px] relative -top-[12px] ms-4'>
+                        <FaTint className="mr-[4px] text-sky-300 text-[14px]" />
+                        {currentDew}<sup className='text-[8px] relative -top-[3px]'>°C</sup>
+                      </span>
                     </span>
-                  </span>
-                  <span className='flex items-center w-full sm:w-auto text-[13px] mt-4 relative ms-3'>
-                    Min {Math.round(currentMinTemp)} <sup className='text-[5px] relative -top-[4px]'>°C</sup> 
-                    <FaThermometerHalf className="mr-1 text-sky-300 relative top-[0px]" /> 
-                    <span className='mx-3'>-</span> 
-                    Max {Math.round(currentMaxTemp)}<sup className='text-[5px] relative -top-[4px]'>°C</sup>
-                    <FaThermometerHalf className="mr-1 text-rose-500 relative top-[0px]" />
-                  </span>
-                </p>
-                <p className='px-3 text-3xl'>{currentConditions}</p>
-                <div className='flex flex-col items-start px-3'>
-                <p className="text-slate-200 flex items-center mb-1">
-                  <FaWind className="mr-2 text-sky-300" /> {windSpeed} km/h
-                </p>
-                <p className="text-slate-200 flex items-center mb-1">
-                  <FaTint className="mr-2 text-sky-300" /> {humidity}%
-                </p>
-                <p className="text-slate-200 flex items-center justify-start relative -left-[12px]">
-                  <img src={`${process.env.PUBLIC_URL}/cloudy-day-1.svg`} className='w-9 h-8 object-cover' alt="cloudcover" /> {cloudCover}%
-                </p>
+                    <span className='flex items-center w-full sm:w-auto text-[13px] mt-4 relative ms-3'>
+                      Min {Math.round(currentMinTemp)} <sup className='text-[5px] relative -top-[4px]'>°C</sup> 
+                      <FaThermometerHalf className="mr-1 text-sky-300 relative top-[0px]" /> 
+                      <span className='mx-3'>-</span> 
+                      Max {Math.round(currentMaxTemp)}<sup className='text-[5px] relative -top-[4px]'>°C</sup>
+                      <FaThermometerHalf className="mr-1 text-rose-500 relative top-[0px]" />
+                    </span>
+                  </p>
+                  <p className='px-3 text-3xl'>{currentConditions}</p>
+                  <div className='flex flex-col items-start px-3'>
+                  <p className="text-slate-200 flex items-center mb-1">
+                    <FaWind className="mr-2 text-sky-300" /> {windSpeed} km/h
+                  </p>
+                  <p className="text-slate-200 flex items-center mb-1">
+                    <FaTint className="mr-2 text-sky-300" /> {humidity}%
+                  </p>
+                  <p className="text-slate-200 flex items-center justify-start relative -left-[12px]">
+                    <img src={`${process.env.PUBLIC_URL}/cloudy-day-1.svg`} className='w-9 h-8 object-cover' alt="cloudcover" /> {cloudCover}%
+                  </p>
+                  </div>
+                </div>
+                <div className='mt-2 w-full'>
+                  <p className="text-slate-400 flex items-center text-[12px]">
+                    ***{weatherDesc}
+                  </p>
                 </div>
               </div>
-              <div className='mt-2 w-full'>
-                <p className="text-slate-400 flex items-center text-[12px]">
-                  ***{weatherDesc}
-                </p>
-              </div>
-            </div>
           </div>
           <div className="flex flex-wrap gap-4 mt-4">
-            <div className='flex flex-wrap md:w-3/5'>
-              <h2 className='text-3xl font-bold p-5'>15-Days Forcast</h2>
-            {weather &&
-            weather.days.slice(1, 16).map((day, index) => (
-              <div key={index} className="bg-slate-950/90 p-0 shadow-md flex-grow basis-full mb-5 select-none rounded-lg overflow-hidden">
-                <p className="text-slate-200 font-bold p-4 border-b-2 border-slate-800 cursor-pointer mb-0" onClick={() => handleDayClick(index)}>
-                  {moment(day.datetime).format('MMM D')}
-                </p>
-                
-                  <div className={`transition-all duration-100 ${selectedDayIndex === index ? 'max-h-full opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}>
-                    <table className="text-slate-300 text-start w-full border-spacing-1 table-auto rounded-b-md">
-                      <thead className='text-[13px]'>
-                        <tr>
-                          <th className='text-start bg-slate-900 py-2 px-3 font-medium'>Feels Like Max</th>
-                          <th className='text-start bg-slate-900 py-2 px-3 font-medium'>Feels Like Min</th>
-                          <th className='text-start bg-slate-900 py-2 px-3 font-medium'>Humidity %</th>
-                          <th className='text-start bg-slate-900 py-2 px-3 font-medium'>Wind Speed km/h</th>
-                          <th className='text-start bg-slate-900 py-2 px-3 font-medium'>Weather Condition</th>
-                        </tr>
-                      </thead>
-                      <tbody className='bg-slate-100/20 text-[13px] font-extrabold'>
-                        <tr>
-                          <td className='py-2 px-3 bg-slate-700'>{Math.round(day.feelslikemax)}°C</td>
-                          <td className='py-2 px-3 bg-slate-700'>{Math.round(day.feelslikemin)}°C</td>
-                          <td className='py-2 px-3 bg-slate-700'>{day.humidity}</td>
-                          <td className='py-2 px-3 bg-slate-700'>{day.windspeed}</td>
-                          <td className='py-2 px-3 bg-slate-700'>{day.conditions}</td>
-                        </tr>
-                      </tbody>
-                    </table>
+              <div className='flex flex-wrap justify-between items-start overflow-hidden gap-4'>
+                <h2 className='text-3xl font-bold p-5 basis-full'>15-Days 24x7 Forcast</h2>
+                {weather &&
+                weather.days.slice(0, 15).map((day, index) => (
+                  <div key={index} className="bg-slate-950/50 w-full p-0 shadow-md select-none rounded-lg overflow-hidden">
+                    <p className="text-slate-200 flex justify-between font-bold p-4 border-slate-800 cursor-pointer mb-0" onClick={() => handleDayClick(index)}>
+                      <span>{moment(day.datetime).format('MMM D')}</span>
+                      <span><FaThermometerHalf className="mr-[0px] -mt-[3px] text-rose-500 inline-block text-[14px]" /> {Math.round(day.feelslike)}<sup className='text-[7px]'>°C</sup></span>
+                    </p>
+                    <div className={`overflow-x-auto w-full transition-all duration-100 ${selectedDayIndex === index ? 'max-h-full opacity-100' : 'max-h-0 opacity-0'}`}>
+                      <table className="text-slate-400 text-start w-full table-fixed rounded-b-md">
+                        <thead className='text-[10px] md:text-[15px]'>
+                          <tr>
+                            <th className='text-start bg-gray-950 py-2 px-3 font-medium w-[50px]'>Time</th>
+                            <th className='text-start bg-gray-950 py-2 px-3 font-medium w-[70px]'>Feels Like</th>
+                            <th className='text-start bg-gray-950 py-2 px-3 font-medium w-[80px]'>Humidity </th>
+                            <th className='text-start bg-gray-950 py-2 px-3 font-medium w-[90px]'>Wind Speed</th>
+                            <th className='text-start bg-gray-950 py-2 px-3 font-medium w-[150px]'>Weather Condition</th>
+                          </tr>
+                        </thead>
+                        <tbody className='bg-slate-100/20 text-[10px] md:text-[15px] font-extrabold divide-y divide-slate-800'>
+                          {day && day.hours.slice(0, 25).map((hour, hourIndex) => (
+                            <tr key={hourIndex} className={`${hourIndex === currentHour ? ' bg-gradient-to-r from-sky-600 to-yellow-700 text-slate-900' : ''}`}>
+                              <td className='py-2 px-3  w-[50px]'>
+                              {hourIndex}:00
+                              </td>
+                              <td className='py-2 px-3  w-[70px]'><FaThermometerHalf className="mr-[2px] -mt-[1px] text-rose-500 inline-block text-[10px] md:text-[15px]" /> {Math.round(hour.feelslike)}<sup className='text-[7px]'>°C</sup></td>
+                              <td className='py-2 px-3  w-[80px]'><FaTint className="mr-[3px] -mt-[2px] text-sky-300 inline-block text-[10px] md:text-[15px]" /> {hour.humidity} %</td>
+                              <td className='py-2 px-3  w-[90px]'><FaWind className="mr-[3px] -mt-[2px] text-sky-300 inline-block text-[10px] md:text-[15px]" /> {hour.windspeed} <sup className='font-light text-[7px]'>km/h</sup></td>
+                              <td className='py-2 px-3  w-[150px] text-start'>{hour.conditions}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
                   </div>
-           
+                ))}
               </div>
-            ))}
-            </div>
-            <div className='md:w-2/5'>
+              <div className='md:w-2/5'>
 
-            </div>
+              </div>
           </div>
         </>
       )}
